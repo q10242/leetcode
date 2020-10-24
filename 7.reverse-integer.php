45dbbe5b83b5a6e6b57b99882223e59f9757f1f3
q10242 <q10242@gmail.com>
2020-10-24 18:32:13 +0800

    7.revers-integer.php

--- 7.reverse-integer.php
+++ 7.reverse-integer.php
@@ -12,6 +12,24 @@ class Solution {
      * @return Integer
      */
     function reverse($x) {
+
+        $symble= 1;
+        if($x < 0) {
+            $symble = -1;
+        }
+
+        $rev_of_x = (
+            (int)
+                (strrev(
+                    (String)
+                        (abs($x))
+                    )
+                )
+            );
+        if($rev_of_x > 2147483647 || $rev_of_x < -2147483648) {
+            return 0;
+        }
+        return $symble* $rev_of_x;
         
     }
 }
