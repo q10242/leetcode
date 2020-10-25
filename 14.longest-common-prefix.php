a29c4e78a4c18cd6201743441a13d8e96a99568f
q10242 <q10242@gmail.com>
2020-10-25 17:42:53 +0800

    14. Longest Common Prefix

--- 14.longest-common-prefix.php
+++ 14.longest-common-prefix.php
@@ -3,7 +3,7 @@
  *
  * [14] Longest Common Prefix
  */
-
+<?
 // @lc code=start
 class Solution {
 
@@ -12,7 +12,35 @@ class Solution {
      * @return String
      */
     function longestCommonPrefix($strs) {
-        
+        $prefix  = "";
+        if(count($strs) ==1) {
+            return $strs[0];
+        }        
+        for ($i = 0; $i< strlen($strs[0]); $i++) {
+            $temp = "";
+            $add = true;
+            $first_time = true;
+            foreach ($strs as $str) {
+                if($first_time) { 
+                    $first_time = false;
+                    continue;
+                }
+                if($strs[0][$i] ==  $str[$i] ) {
+                    $temp = $str[$i];
+                } else {
+                    $add = ($add && false);
+                }
+            }
+
+            if($add) {
+                $prefix .= $temp;
+            } else {
+                break;
+            }
+
+        }    
+
+        return $prefix;
     }
 }
 // @lc code=end
